6fc37e3da20a8f9db24ff0cf1f2a3c3e0f5274c4
Tempeus <36718822+Tempeus@users.noreply.github.com>
2023-07-07 19:34:47 -0400

    is-subsequence
    Runtime: 40ms beats 92.69
    Memory: 16.3MB beats 64.88

--- LeetCode 75/Two Pointers/is-subsequence.py
+++ LeetCode 75/Two Pointers/is-subsequence.py
@@ -1,2 +1,10 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
+        match_index = 0
+        max_index = len(s)
+        matches = 0
+        for letter in t:
+            if match_index < max_index and letter == s[match_index]:
+                matches += 1
+                match_index += 1
+        return matches == max_index
